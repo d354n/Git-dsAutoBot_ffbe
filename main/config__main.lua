cfcc7aa8baa8525854edf9aca228edfacda98e4a
d354n <denny.sanantha@gmail.com>
2017-03-03 10:31:01 +0700

    Main Script
    Main Script for running the auto games

---
+++ main/config__main.lua
@@ -0,0 +1,10 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: denny
+-- Date: 1/29/2017
+-- Time: 11:00 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+dofile(scriptPath().."../custom/ffbe/config__main.lua")
+
